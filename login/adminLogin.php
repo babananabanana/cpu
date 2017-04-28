f2ef15e7e7481886c0df0cb84493d4e0b406052c
babananabanana <cshyxxxl@163.com>
2017-04-28 16:32:31 +0800

    cpu

---
+++ login/adminLogin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xonls
+ * Date: 17-4-27
+ * Time: 下午4:00
+ */
---
+++ login/js/login.js
@@ -0,0 +1,16 @@
+/**
+ * Created by xonls on 17-4-27.
+ */
+<script>
+function userLogin()
+{
+    document.form1.action="userLogin.php"
+    document.form1.submit();
+}
+
+function adminLogin()
+{
+    document.form1.action="adminLogin.php"
+    document.form1.submit();
+}
+</script>
